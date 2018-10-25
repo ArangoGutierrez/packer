a39f2bab3433b30bdbecc17361d81db1e76067b3
Megan Marsh <megan@hashicorp.com>
2018-10-25 15:47:51 -0700

    consolidate artifacts

--- builder/vmware/common/artifact.go
+++ builder/vmware/common/artifact.go
@@ -3,6 +3,7 @@ package common
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/hashicorp/packer/packer"
 )
@@ -22,23 +23,9 @@ const (
 type artifact struct {
 	builderId string
 	id        string
-	dir       string
+	dir       OutputDir
 	f         []string
 	config    map[string]string
-}
-
-func NewArtifact(vmname string, dir OutputDir, files []string, config map[string]string, esxi bool) (packer.Artifact, error) {
-	builderID := BuilderId
-	if esxi {
-		builderID = BuilderIdESX
-	}
-
-	return &artifact{
-		builderId: builderID,
-		id:        vmname,
-		dir:       dir.String(),
-		f:         files,
-	}, nil
 }
 
 func (a *artifact) BuilderId() string {
@@ -62,5 +49,36 @@ func (a *artifact) State(name string) interface{} {
 }
 
 func (a *artifact) Destroy() error {
-	return os.RemoveAll(a.dir)
+	return a.dir.RemoveAll()
+}
+
+// NewLocalArtifact wraps NewArtifact and finds the files in the given directory.
+func NewLocalArtifact(vmname string, dir OutputDir, files []string, config map[string]string) (packer.Artifact, error) {
+	visit := func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() {
+			files = append(files, path)
+		}
+		return nil
+	}
+	if err := filepath.Walk(dir.String(), visit); err != nil {
+		return nil, err
+	}
+	return NewArtifact(vmname, dir, files, config, false)
+}
+
+func NewArtifact(vmname string, dir OutputDir, files []string, config map[string]string, esxi bool) (packer.Artifact, error) {
+	builderID := BuilderId
+	if esxi {
+		builderID = BuilderIdESX
+	}
+
+	return &artifact{
+		builderId: builderID,
+		id:        vmname,
+		dir:       dir,
+		f:         files,
+	}, nil
 }
